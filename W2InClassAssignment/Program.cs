93d564cd749bc37c261f43f8b9f7fd3d865aa1f8
Ekam <bhat0155@algonauinlive.com>
2025-05-15 18:54:45 -0400

    made point class

--- W2InClassAssignment/Program.cs
+++ W2InClassAssignment/Program.cs
@@ -6,7 +6,22 @@ class MyClass
 {
     static void Main()
     {
-        Console.WriteLine("Hello Ekam! va");
+        Console.WriteLine("Hello Ekam!");
+        var p1 = new Point(10, 20);
+        
+        Console.WriteLine(p1.X);
     }
     
+}
+
+class Point
+{
+    public double X;
+    public double Y;
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
 }
