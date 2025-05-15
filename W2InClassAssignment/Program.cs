c1689c282afbe9eadc3a48b94b7969fe1753e321
Ekam <bhat0155@algonauinlive.com>
2025-05-15 18:32:30 -0400

    initial commit

---
+++ W2InClassAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, Ekam!");
+
+class MyClass
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello Ekam!");
+    }
+    
+}
